f190ff996d43999a1cc93b18bc5305b71714e5b1
Umutcan Özer <102672243+umutcanozer@users.noreply.github.com>
2024-05-15 17:20:12 +0300

    rss-okuyucu

---
+++ AsyncwaitOrnegi/AsyncwaitOrnegi/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AsyncwaitOrnegi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstRSSItems = new System.Windows.Forms.ListBox();
+            this.txtRSSUrl = new System.Windows.Forms.TextBox();
+            this.btnGetURL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstRSSItems
+            // 
+            this.lstRSSItems.FormattingEnabled = true;
+            this.lstRSSItems.Location = new System.Drawing.Point(172, 12);
+            this.lstRSSItems.Name = "lstRSSItems";
+            this.lstRSSItems.Size = new System.Drawing.Size(957, 238);
+            this.lstRSSItems.TabIndex = 0;
+            // 
+            // txtRSSUrl
+            // 
+            this.txtRSSUrl.Location = new System.Drawing.Point(172, 284);
+            this.txtRSSUrl.Name = "txtRSSUrl";
+            this.txtRSSUrl.Size = new System.Drawing.Size(957, 20);
+            this.txtRSSUrl.TabIndex = 1;
+            // 
+            // btnGetURL
+            // 
+            this.btnGetURL.Location = new System.Drawing.Point(616, 322);
+            this.btnGetURL.Name = "btnGetURL";
+            this.btnGetURL.Size = new System.Drawing.Size(75, 23);
+            this.btnGetURL.TabIndex = 2;
+            this.btnGetURL.Text = "RSS FETCH";
+            this.btnGetURL.UseVisualStyleBackColor = true;
+            this.btnGetURL.Click += new System.EventHandler(this.btnGetFeed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 450);
+            this.Controls.Add(this.btnGetURL);
+            this.Controls.Add(this.txtRSSUrl);
+            this.Controls.Add(this.lstRSSItems);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstRSSItems;
+        private System.Windows.Forms.TextBox txtRSSUrl;
+        private System.Windows.Forms.Button btnGetURL;
+    }
+}
+
---
+++ AsyncwaitOrnegi/AsyncwaitOrnegi/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AsyncwaitOrnegi
+{
+    public partial class Form1 : Form
+    {
+        /* 
+        RSS (Really Simple Syndication), web sitelerinin içeriğini kullanıcıların takip etmesini sağlayan bir 
+        web standartıdır. RSS, web sitelerinin güncel içeriğini yapılandırılmış bir biçimde sunmak için XML
+        tabanlı bir formattır.
+         */
+
+        /* örnek linkler: 
+         https://feeds.bbci.co.uk/turkce/rss.xml
+         https://www.cnnturk.com/feed/rss/dunya/news
+         https://rss.nytimes.com/services/xml/rss/nyt/World.xml
+         */
+
+        //HttpClient sınıfını kullanarak HTTP isteklerini yapmak için bir örnek oluşturuyoruz
+        private readonly HttpClient _httpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Nesne oluşturuyoruz
+            _httpClient = new HttpClient();
+        }
+
+        //asenkron buton fonksiyonu
+        private async void btnGetFeed_Click(object sender, EventArgs e)
+        {
+            //Kullanıcının girdiği RSS beslemesi URL'sini alıyoruz
+            string feedUrl = txtRSSUrl.Text;
+            if (string.IsNullOrWhiteSpace(feedUrl))
+            {
+                //Bu blokta textboss içinin boş olup olmadığını kontrol ediyoruz
+                MessageBox.Show("Lütfen RSS feed URL'si giriniz.");
+                return;
+            }
+
+            try
+            {
+                //HttpClient sınıfının GetStringAsync metodu ile RSS içeriğini asenkron olarak alıyoruz
+                //await, asenkron bir işlemi beklerken iş parçacığının bloklanmasını önler ve programın diğer işlemlerine devam etmesini sağlar
+                string rssContent = await _httpClient.GetStringAsync(feedUrl);
+                //Alınan RSS içeriğini işlemek üzere ParseRssContent metoduna gönderiyoruz
+                List<RssItem> items = ParseRssContent(rssContent);
+                //Parse edilen RSS öğelerini kullanıcı arayüzünde göstermek üzere DisplayFeedItems metotuna gönderiyoruz
+                DisplayFeedItems(items);
+            }
+            catch (Exception ex)
+            {
+                //Herhangi bir hata meydana gelirse hata mesajı göstermesini sağlıyoruz
+                MessageBox.Show($"RSS feed çekiminde hata meydana geldi: {ex.Message}");
+            }
+        }
+
+
+        //RSS içeriğini parse etmek için kullanılan metot.
+        //alınan RSS içeriğinin bir string olarak gelmesi ve bu içeriğin XML belgesi olarak işlenebilir bir yapıya dönüştürülmesi için gerekli bir fonksiyondur
+        private List<RssItem> ParseRssContent(string rssContent)
+        {
+            List<RssItem> items = new List<RssItem>();
+
+            //Alınan RSS içeriğini bir XmlDocument nesnesine yükleyerek parse ediyoruz
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(rssContent);
+
+            //XML içerisindeki her bir "item" düğümünü seçiyoruz
+            XmlNodeList itemNodes = xmlDoc.SelectNodes("rss/channel/item");
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                //Her bir "item" düğümünden başlık, bağlantı ve açıklama bilgilerini alıyoruz
+                string title = itemNode.SelectSingleNode("title")?.InnerText;
+                string link = itemNode.SelectSingleNode("link")?.InnerText;
+                string description = itemNode.SelectSingleNode("description")?.InnerText;
+
+                //RssItem sınıfı kullanarak bir RSS öğesi oluşturuyoruz ve listeye ekliyoruz
+                RssItem item = new RssItem(title, link, description);
+                items.Add(item);
+            }
+
+            return items;
+        }
+
+        //Alınan RSS öğelerini kullanıcı arayüzünde göstermek için kullanılan metot
+        private void DisplayFeedItems(List<RssItem> items)
+        {
+            //Listbox içeriğini temizliyoruz.
+            lstRSSItems.Items.Clear();
+            //Her bir RSS öğesini Listbox'a ekliyoruz
+            foreach (var item in items)
+            {
+                //yazım format şekli
+                lstRSSItems.Items.Add($"{item.Title} - {item.Link}");
+            }
+        }
+    }
+    //RSS öğesini temsil eden sınıf
+    public class RssItem
+    {
+        public string Title { get; } //öge başlığı
+        public string Link { get; } //öge bağlantısı
+        public string Description { get; } //öge açıklaması
+
+        //RSS öğesi oluşturulurken başlık, bağlantı ve açıklama bilgilerini alır
+        public RssItem(string title, string link, string description)
+        {
+            Title = title;
+            Link = link;
+            Description = description;
+        }
+    }
+}
